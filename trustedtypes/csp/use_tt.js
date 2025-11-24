aa69e2d2efee37a1fab87c64017f14a5ff3a3e29
Ally Nisenoff <nisenoff@cs.uchicago.edu>
2025-11-24 07:47:26 -0500

    tt init

--- trustedtypes/csp/use_tt.js
+++ trustedtypes/csp/use_tt.js
@@ -7,7 +7,7 @@ const escapeHTMLPolicy = trustedTypes.createPolicy("3pEscapePolicy", {
             .replace(/'/g, "&apos;"),
 });
 
-let el = document.getElementById("myDiv2");
+let el2 = document.getElementById("myDiv2");
 const escaped = escapeHTMLPolicy.createHTML("<img src=x onerror=alert(1)>");
 console.log(escaped instanceof TrustedHTML); // true
-el.innerHTML = escaped;
+el2.innerHTML = escaped;
--- trustedtypes/no_tt.js
+++ trustedtypes/no_tt.js
@@ -1,5 +1,5 @@
 
-let el = document.getElementById("myDiv2");
+let el2 = document.getElementById("myDiv2");
 const text = "<img src=x onerror=alert(1)>";
 console.log(text instanceof TrustedHTML); // true
-el.innerHTML = text;
+el2.innerHTML = text;
