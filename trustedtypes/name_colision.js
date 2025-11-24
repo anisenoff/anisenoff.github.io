3cc7bcd70c3d47a42607464c103f4f7f94b203ba
Ally Nisenoff <nisenoff@cs.uchicago.edu>
2025-11-24 10:20:48 -0500

    name

--- trustedtypes/name_colision.js
+++ trustedtypes/name_colision.js
@@ -1,8 +1,13 @@
-var escapeHTMLPolicy = trustedTypes.createPolicy("EscapePolicy", {
-    createHTML: (string) => string
+const escapeHTMLPolicy2 = trustedTypes.createPolicy("p3EscapePolicy", {
+    createHTML: (string) =>
+        string
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&apos;"),
 });
 
 let el2 = document.getElementById("myDiv2");
-const escaped2 = escapeHTMLPolicy.createHTML("<img src=x onerror=alert(2)>");
+const escaped2 = p3escapeHTMLPolicy2.createHTML("<img src=x onerror=alert(2)>");
 console.log(escaped2 instanceof TrustedHTML); // true
 el2.innerHTML = escaped2;
